671d717861532d0caa64927b33b9569e7ffab2de
Kaushik Kumar Jha <125187850+Kaushikjha007@users.noreply.github.com>
2024-09-03 19:35:14 +0530

    Update Home Page JS.js

--- Kaushik Grocery Store/Home Page JS.js
+++ Kaushik Grocery Store/Home Page JS.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "30 Aug 2024";
+const newYears = "31 oct 2024";
 
 function countdown() {
     const newYearsDate = new Date(newYears);
